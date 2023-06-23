ca5d767794c1301cf83e8b3fdaef2be6dc8a95ff
mrshukhratbek <shukhratbekqobulov@gmail.com>
2023-06-23 11:10:02 +0500

    feat: finish layout

--- src/components/layout/footer/footer.vue
+++ src/components/layout/footer/footer.vue
@@ -1,9 +1,45 @@
-<script></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+
+import Instagram from '@/components/icons/instagram.vue'
+import Telegram from '@/components/icons/telegram.vue'
+import Twitter from '@/components/icons/twitter.vue'
+</script>
 
 <template>
-  <footer>
-    Header
+  <footer class="text-white py-7">
+    <div class="container mx-auto py-4 px-4 sm:px-6 lg:px-8 flex flex-col">
+      <div class="flex items-center mb-6 justify-center">
+        <a href="#" class="mr-6">
+          <svg class="h-6 w-6 text-white" fill="currentColor" viewBox="0 0 24 24">
+            <Instagram />
+          </svg>
+        </a>
+        <a href="#" class="mr-6">
+          <svg class="h-6 w-6 text-white" fill="currentColor" viewBox="0 0 24 24">
+            <Telegram />
+          </svg>
+        </a>
+        <a href="#">
+          <svg class="h-6 w-6 text-white" fill="currentColor" viewBox="0 0 24 24">
+            <Twitter />
+          </svg>
+        </a>
+      </div>
+
+      <div class="flex items-center justify-center">
+        <RouterLink to="/about" class="text-gray-500">Biz haqimizda</RouterLink>
+        <div class="h-3 bg-gray-500 mx-5 w-px"></div>
+        <RouterLink to="/rules" class="text-gray-500">Foydalanish qoidalari</RouterLink>
+        <div class="h-3 bg-gray-500 mx-5 w-px"></div>
+        <RouterLink to="/questions" class="text-gray-500">Savol va takliflar</RouterLink>
+      </div>
+    </div>
   </footer>
 </template>
 
-<style></style>
+<style>
+footer {
+  background-color: #1c1c1c;
+}
+</style>
--- src/components/layout/header/header.vue
+++ src/components/layout/header/header.vue
@@ -1,9 +1,71 @@
-<script></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+
+import Logo from '@/components/icons/logo.vue'
+import SearchIcon from '@/components/icons/search-icon.vue'
+</script>
 
 <template>
-  <header>
-    Header
+  <header class="bg-header-color text-white">
+    <div class="bg-orange-line"></div>
+
+    <div class="container mx-auto py-4 flex flex-wrap items-center justify-between">
+      <!-- Logo -->
+      <div class="flex items-center">
+        <Logo />
+      </div>
+
+      <!-- Navigation Links -->
+      <nav>
+        <ul class="flex flex-wrap space-x-6 mt-4 sm:mt-0 sm:ml-auto justify-center">
+          <li>
+            <RouterLink to="/" class="hover:text-white text-gray-400">
+              Zamonaviy bilimlar
+            </RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/grants" class="hover:text-white text-gray-400">Grantlar</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/languages" class="hover:text-white text-gray-400"
+              >Chet tillari</RouterLink
+            >
+          </li>
+          <li>
+            <RouterLink to="/news" class="hover:text-white text-gray-400">Yangiliklar</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/conferences" class="hover:text-white text-gray-400">
+              Tadbir va konferensiyalar
+            </RouterLink>
+          </li>
+        </ul>
+      </nav>
+
+      <!-- Search Input -->
+      <div class="relative mt-4 sm:mt-0">
+        <SearchIcon />
+        <input
+          type="text"
+          class="bg-gray-800 text-white rounded-lg py-2 px-10 focus:outline-none focus:ring-2 focus:ring-blue-500 w-305"
+          placeholder="Search"
+        />
+      </div>
+    </div>
   </header>
 </template>
 
-<style></style>
+<style>
+.bg-header-color {
+  background-color: #0f0f0f;
+}
+
+.bg-orange-line {
+  height: 2px;
+  background-color: #ff5b25;
+}
+.w-305 {
+  width: 305px;
+  background-color: #2b2b2b;
+}
+</style>
